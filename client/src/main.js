9184b30131e4f8160ce1e827c51f7ac0db21b1fd
Peter <Peterlee2094@gmail.com>
2023-08-30 18:22:47 -0500

    added default home page

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,8 @@
 import { menuSelector } from "./components/navMenu/NavMenu";
 import { createProjectPage } from "./components/pages/project/project";
-
+import { createPage } from "./global/Pages";
 function initalLoad() {
+  createPage("/", "homeSession", () => {});
   createProjectPage();
 }
 
