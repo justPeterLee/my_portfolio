5661857825f1b8cab02c8e122a81f646845aced1
Peter <Peterlee2094@gmail.com>
2023-08-30 18:19:31 -0500

    added pageObj to updater

--- client/src/utils/updater/updater.js
+++ client/src/utils/updater/updater.js
@@ -1,4 +1,7 @@
+import { pagesObj } from "../../global/Pages";
+
 export const updater = () => {
   const location = window.location.pathname;
   console.log(location);
+  console.log(pagesObj);
 };
