dbeceafc7c37eb390620f883f2da7cbce163f05c
Peter <Peterlee2094@gmail.com>
2023-08-30 14:01:45 -0500

    implemented SPA routing

---
+++ client/src/utils/router/router.js
@@ -0,0 +1,31 @@
+// page routing system
+const links = document.getElementsByClassName("page-links");
+const linkArr = [...links];
+
+linkArr.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    urlRoute();
+  });
+});
+
+const urlRoute = (event) => {
+  event = event || window.event;
+  event.preventDefault();
+
+  window.history.pushState({}, "", event.target.href);
+  urlLocationHandler();
+};
+
+const urlLocationHandler = async () => {
+  let location = window.location.pathname;
+  if (location.length === 0) {
+    location = "/";
+  }
+};
+
+window.onpopstate = urlLocationHandler;
+
+window.route = urlRoute;
+
+urlLocationHandler();
