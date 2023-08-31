64e5808fb44aebb012269d1acb01e3ec8fd60b9c
Peter <Peterlee2094@gmail.com>
2023-08-30 19:03:30 -0500

    add properties to menu generator

--- client/src/components/navMenu/generateNavMenu.js
+++ client/src/components/navMenu/generateNavMenu.js
@@ -1,5 +1,12 @@
 import { pagesObj } from "../../global/Pages";
 
 export function generateNavMenu() {
-  console.log("menu: ", pagesObj);
+  const menuContainer = document.querySelector("#nav-menu-container");
+  const pageKeys = Object.keys(pagesObj);
+
+  console.log(pageKeys);
+
+  for (let i = 0; i < pageKeys.length; i++) {
+    console.log(pageKeys[i]);
+  }
 }
