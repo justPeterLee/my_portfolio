fd386a70b239d5ce29099c5c5f5aa8b67ba50ba0
Peter <Peterlee2094@gmail.com>
2023-08-30 19:47:22 -0500

    create rendering method to render class

--- client/src/global/Rendered.js
+++ client/src/global/Rendered.js
@@ -10,6 +10,10 @@ class Rendered {
   set Rendered(name) {
     this._Rendered = name;
   }
+
+  rendering(pageInstance) {
+    console.log(pageInstance);
+  }
 }
 
 export const rendered = new Rendered();
--- client/src/utils/updater/updater.js
+++ client/src/utils/updater/updater.js
@@ -1,9 +1,12 @@
 import { pagesObj } from "../../global/Pages";
 import { menuSelector } from "../../components/navMenu/NavMenu";
+import { rendered } from "../../global/Rendered";
+
 export const updater = () => {
   const location = window.location.pathname;
+  const pageInstance = pagesObj[location];
 
   menuSelector();
-  console.log(location);
-  console.log(pagesObj);
+
+  rendered.rendering(pageInstance);
 };
