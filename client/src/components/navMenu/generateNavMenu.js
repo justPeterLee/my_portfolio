bdaca664d5e40a82d3d3688083d90d1f399ed22e
Peter <Peterlee2094@gmail.com>
2023-08-30 19:25:38 -0500

    added menu selector to updator

--- client/src/components/navMenu/generateNavMenu.js
+++ client/src/components/navMenu/generateNavMenu.js
@@ -4,10 +4,7 @@ export function generateNavMenu() {
   const menuContainer = document.querySelector("#nav-menu-container");
   const pageKeys = Object.keys(pagesObj);
 
-  console.log(pageKeys);
-
   for (let i = 0; i < pageKeys.length; i++) {
-    console.log(pageKeys[i]);
     const textContainer = document.createElement("span");
     textContainer.id = "link-not-selected";
     textContainer.className = "nav-menu-text-container";
--- client/src/utils/updater/updater.js
+++ client/src/utils/updater/updater.js
@@ -1,7 +1,9 @@
 import { pagesObj } from "../../global/Pages";
-
+import { menuSelector } from "../../components/navMenu/NavMenu";
 export const updater = () => {
   const location = window.location.pathname;
+
+  menuSelector();
   console.log(location);
   console.log(pagesObj);
 };
