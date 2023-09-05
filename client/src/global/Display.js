a198fdd1790c113c8a3b3d05df1f2488bfa1201f
Peter <Peterlee2094@gmail.com>
2023-09-05 18:26:41 -0500

    remove page transition and animation

--- client/src/global/Display.js
+++ client/src/global/Display.js
@@ -45,76 +45,12 @@ export class Display {
 
   showComponent(pageInstance, render) {
     const isRender = render || false;
-
-    // same page
-    if (this.session === pageInstance.sessionKey) {
-      console.log("same page");
-      return;
-    }
-
-    // if already displaying
-    if (this.isDisplay && this.Display) {
-      // console.log("something is displaying:");
-      // hide current display
-      this.hideComponent(this.Display).then(() => {
-        console.log("DONE hiding");
-        if (isRender) {
-          this.renderComponent(pageInstance);
-        } else {
-          pageInstance.show();
-        }
-      });
-    } else {
-      // console.log("nothing is displaying");
-      if (isRender) {
-        this.renderComponent(pageInstance);
-      } else {
-        pageInstance.show();
-      }
-    }
-
+    console.log("old: ", this.Display);
+    console.log("new: ", pageInstance);
     this.update(pageInstance);
-    // // if there is already something rendered and isnt the same render
-    // if (this.isDisplay && this.Display) {
-    //   // this.hideComponent(pageInstance)
-    //   console.log("must hide current session first:", this.Display);
-    //   this.hideComponent(this.Display).then(() => {
-    //     // pageInstance.show();
-    //     console.log("now show");
-    //     if (isRender) {
-    //       console.log("rendering");
-    //       pageInstance.initial(pageInstance.parent).then(() => {
-    //         rendered.rendering(pageInstance);
-    //       });
-    //     } else {
-    //       console.log("Now avalible to show:", this.Display);
-    //       pageInstance.show();
-    //     }
-    //   });
-    //   // run hideComponent
-    // } else {
-    //   if (isRender) {
-    //     console.log("rendering");
-    //     pageInstance.initial(pageInstance.parent).then(() => {
-    //       rendered.rendering(pageInstance);
-    //     });
-    //   } else {
-    //     console.log("Now avalible to show:", this.Display);
-    //     pageInstance.show();
-    //   }
-    // }
   }
 
-  hideComponent(pageInstance) {
-    return new Promise((resolve, reject) => {
-      // console.log("something able to hide", pageInstance);
-      pageInstance.hide();
-      this.reset();
-      setTimeout(() => {
-        resolve();
-      }, 500);
-    });
-  }
+  hideComponent(pageInstance) {}
 
   update(pageInstance) {
     this.setIsDisplay = true;
--- client/src/utils/animation/animation.js
+++ client/src/utils/animation/animation.js
@@ -1,33 +1,35 @@
 import gsap from "gsap";
+import { display } from "../../global/Display";
 // need to accept moving element and timeline
-const tl = gsap.timeline();
+
+const globalTL = display.timeline;
 function defaultShow(element) {
-  tl.from(`#${element.id}`, {
+  globalTL.from(`#${element.id}`, {
     x: 200,
     opacity: 0,
     direction: 1,
-    onStart: () => {
-      element.style = {};
-      element.parentElement.style.display = {};
-    },
+    // onStart: () => {
+    //   element.style = {};
+    //   element.parentElement.style.display = {};
+    // },
   });
 }
 
 function defaultHide(element) {
-  tl.to(`#${element.id}`, {
+  globalTL.to(`#${element.id}`, {
     y: -200,
     opacity: 0,
     duration: 0.5,
-    onStart: () => {
-      console.log("START");
-    },
-    onComplete: () => {
-      element.style = {};
-      // console.log(element.parentElement.nodeName);
-      element.parentElement.style.display = "none";
-      // element.style.opacity = "100%";
-      console.log("end");
-    },
+    // onStart: () => {
+    //   console.log("START");
+    // },
+    // onComplete: () => {
+    //   element.style = {};
+    //   // console.log(element.parentElement.nodeName);
+    //   element.parentElement.style.display = "none";
+    //   // element.style.opacity = "100%";
+    //   console.log("end");
+    // },
   });
 }
 
