8ab73c09bf66d1350cf34c0c7528b8c2db1a9036
Peter <Peterlee2094@gmail.com>
2023-09-06 14:10:56 -0500

    update about to use new component instance

--- client/src/components/pages/about/about.js
+++ client/src/components/pages/about/about.js
@@ -4,21 +4,13 @@ import { Components } from "../../../global/Components";
 import { animation } from "../../../utils/animation/animation";
 import { menuAnimation } from "../../../utils/animation/menuAnimation";
 export const createAboutPage = () => {
-  createPage(
-    "/about",
-    "About",
-    "aboutSession",
-    undefined,
-    aboutRender.rendererContainer,
-    { aboutContent },
-    menuAnimation.menuSide
-  );
+  createPage("About", "/about", { aboutContent }, menuAnimation.menuSide);
 };
 
 const aboutContent = new Components(
   "aboutContent",
   aboutRender.aboutContentContainer,
-  animation.defaultShow,
+  null,
   null,
   { isInitial: true }
 );
--- client/src/components/pages/about/renderAbout.js
+++ client/src/components/pages/about/renderAbout.js
@@ -1,12 +1,3 @@
-export function aboutRendererContainer(parent) {
-  return new Promise((resolve, reject) => {
-    const rendererContainer = document.createElement("div");
-    rendererContainer.id = "about-renderer-container";
-    parent.appendChild(rendererContainer);
-    resolve(rendererContainer);
-  });
-}
-
 function aboutContentContainer() {
   const aboutContentContainer = document.createElement("div");
   aboutContentContainer.id = "about-content-container";
@@ -16,13 +7,9 @@ function aboutContentContainer() {
 
   aboutContentContainer.appendChild(text);
 
-  const rendererContainer = document.getElementById("about-renderer-container");
-  rendererContainer.appendChild(aboutContentContainer);
-
   return aboutContentContainer;
 }
 
 export const aboutRender = {
-  rendererContainer: aboutRendererContainer,
   aboutContentContainer,
 };
