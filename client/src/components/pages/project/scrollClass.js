f4cc1ab804ecfdf9650f376f0f036be8e4b4b9ed
Peter <Peterlee2094@gmail.com>
2023-09-22 11:58:50 -0500

    modular tri animation, scroll image buffer

--- client/src/components/pages/project/scrollClass.js
+++ client/src/components/pages/project/scrollClass.js
@@ -1,10 +1,7 @@
 import { gsap } from "gsap";
-import { scrollDescription } from "./renderProject";
 import {
   focusProject,
   blurProject,
-  showTL,
-  hideTL,
 } from "../../../utils/animation/projectAnimation";
 export class projectScroll {
   constructor(scrollContainer, center, images) {
@@ -25,32 +22,41 @@ export class projectScroll {
     this._images.forEach((image) => {
       image.addEventListener("click", (e) => {
         if (!this._mouseMove && !this.isFocus) {
-          this.moveTo(e);
+          this.focus(e);
         }
       });
 
       image.addEventListener("mouseover", (e) => {
         const position = e.target.dataset.position;
-        const tri = document.querySelector(`#image-triangle-${position}`);
-        if (!tri || this.mouseMove || this.isFocus) return;
-
-        gsap.to(`#${tri.id}`, {
-          duration: 0.2,
-          right: tri.dataset.side ? 20 : -20,
-        });
+        this.triAnimation(position, "show");
       });
 
       image.addEventListener("mouseout", (e) => {
         const position = e.target.dataset.position;
-        const tri = document.querySelector(`#image-triangle-${position}`);
-        if (!tri) return;
-
-        gsap.to(`#${tri.id}`, { duration: 0.2, right: 0 });
+        this.triAnimation(position, "hide");
       });
     });
 
     this._moveActive = false;
     this._mouseMove = false;
+    this._scrollCount = 0;
+  }
+
+  triAnimation(position, state) {
+    const tri = document.querySelector(`#image-triangle-${position}`);
+
+    if (state === "show") {
+      if (!tri || this.mouseMove || this.isFocus) return;
+
+      gsap.to(`#${tri.id}`, {
+        duration: 0.2,
+        right: tri.dataset.side ? 20 : -20,
+      });
+    } else if (state === "hide") {
+      if (!tri) return;
+
+      gsap.to(`#${tri.id}`, { duration: 0.2, right: 0 });
+    } else return;
   }
 
   get images() {
@@ -117,6 +123,7 @@ export class projectScroll {
     const height = this._scrollContainer.getBoundingClientRect().height;
     console.log(height);
   }
+
   cachePercent() {
     this._oldPercent = this._currPercent;
   }
@@ -143,12 +150,13 @@ export class projectScroll {
     return new Promise((resolve, reject) => {
       const isManual = manual || false;
       gsap.to(`#${this._scrollContainer.id}`, {
-        transform: `translate(-50%,${newPercentage}%)`,
+        // transform: `translate(-50%,${newPercentage}%)`,
         // translateY: `${newPercentage}%`,
-        // y: `${newPercentage}%`,
+        y: `${newPercentage}%`,
         onUpdate: () => {
           if (isManual) {
-            this.imageAnimation(manual);
+            let imagePercent = this.imagePercentage(manual);
+            this.imageParallax(imagePercent, manual);
           }
           this.centerDetect();
         },
@@ -164,6 +172,7 @@ export class projectScroll {
       objectPosition: `center ${newPercentage}%`,
     });
   }
+
   centerDetect() {
     const centerPos = this._center.getBoundingClientRect();
     this._images.forEach((image) => {
@@ -184,25 +193,20 @@ export class projectScroll {
     });
   }
 
-  moveTo(e) {
-    // console.log();
+  focus(e) {
     if (this.moveActive && this.mouseMove && this.isFocus) return;
-
-    // console.time("animation");
+    this.triAnimation(e.target.dataset.position, "hide");
     this.moveActive = true;
     const element = e.target.parentNode;
     const elementPos = element.getBoundingClientRect();
     const elementCenter = elementPos.top + elementPos.height / 2;
 
-    //   console.log(elementPos.height);
     const centerPos = this._center.getBoundingClientRect();
     const centerCenter = centerPos.top + centerPos.height / 2;
 
     const scrollHeight = this._scrollContainer.getBoundingClientRect().height;
     const distance = centerCenter - elementCenter;
     const distancePercent = (distance / scrollHeight) * 100;
-
-    // console.log(distancePercent);
 
     this.scrollPercent(distancePercent);
 
@@ -211,47 +215,61 @@ export class projectScroll {
       this.cachePercent();
     });
 
-    console.log("moved");
-
     this.whoFocus = { parent: e.target.parentNode, image: e.target };
     this.isFocus = true;
 
     focusProject(this.whoFocus);
   }
 
+  unFocus() {
+    blurProject(this.whoFocus);
+    this.whoFocus = {};
+    this.isFocus = false;
+    this._scrollCount = 0;
+  }
+
   scrollUp() {
-    // this.scrollPercent(-5);
-    // if (showTL.isActive()) return;
-    if (this.isFocus) this.unFocus();
-    this.percent -= 5;
+    if (!this.isFocus) {
+      this.percent -= 5;
 
-    if (this.percent < -100) {
-      this.percent = -99.9;
+      if (this.percent < -100) {
+        this.percent = -99.9;
+      }
+
+      this._currPercent = this.percent;
+      this.defaultImageAnimation();
+      this.scrollAnimation(this.percent);
+    } else {
+      this._scrollCount -= 1;
+
+      if (this._scrollCount <= -6 || this._scrollCount >= 6) {
+        this.unFocus();
+      }
     }
-
-    this._currPercent = this.percent;
-    this.defaultImageAnimation();
-    this.scrollAnimation(this.percent);
   }
 
   scrollDown() {
-    // this.scrollPercent(10);
-    if (this.isFocus) this.unFocus();
-    this.percent += 5;
+    if (!this.isFocus) {
+      this.percent += 5;
 
-    if (this.percent > 0) {
-      this.percent = -0.9;
+      if (this.percent > 0) {
+        this.percent = -0.9;
+      }
+
+      this._currPercent = this.percent;
+      this.defaultImageAnimation();
+      this.scrollAnimation(this.percent);
+    } else {
+      this._scrollCount -= 1;
+      if (this._scrollCount <= -6 || this._scrollCount >= 6) {
+        this.unFocus();
+      }
     }
-
-    this._currPercent = this.percent;
-    this.defaultImageAnimation();
-    this.scrollAnimation(this.percent);
   }
 
   imagePercentage(image) {
     const imagePos = image.getBoundingClientRect();
     const imageCenter = imagePos.top + imagePos.height / 2;
-    // console.log(imageCenter);
 
     const centerPos = this._center.getBoundingClientRect();
     const centerCenter = centerPos.top + centerPos.height / 2;
@@ -262,8 +280,6 @@ export class projectScroll {
     const distancePercent = (distance / scrollHeight) * 100;
 
     const ratioPercent = distancePercent + 50;
-
-    // console.log(ratioPercent);
 
     let imagePercent;
 
@@ -279,40 +295,11 @@ export class projectScroll {
     }
 
     return imagePercent;
-    // this.imageParallax(imagePercent, "checker");
-  }
-
-  imageAnimation(element) {
-    let imagePercent = this.imagePercentage(element);
-    this.imageParallax(imagePercent, element);
   }
 
   defaultImageAnimation() {
     this.images.forEach((element) => {
       this.imageParallax(this.percent + 100, element);
-    });
-  }
-
-  unFocus() {
-    blurProject(this.whoFocus);
-    // hideDescription(this.whoFocus);
-    this.whoFocus = {};
-    this.isFocus = false;
-  }
-
-  generateDescription(parent) {
-    scrollDescription(parent);
-  }
-
-  // testing
-  gsapArray(className) {
-    let newPercent = this.percent;
-    gsap.to(`.${className}`, {
-      duration: 0.5,
-      objectPosition: `center ${newPercent + 100}%`,
-      // onComplete: () => {
-      //   console.log("image parallax COMPLETE");
-      // },
     });
   }
 }
