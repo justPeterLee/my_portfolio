f36b1d20f1cc40823d21975ddf35a4ce1bfa8aa0
Peter <Peterlee2094@gmail.com>
2023-09-23 16:17:18 -0500

    a

--- server/routes/contact.router.js
+++ server/routes/contact.router.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.transporter({
+  host: "smtp.forwardemail.net",
+  port: 587,
+  secure: true,
+  auth: {
+    user: "",
+    pass: "",
+  },
+});
 router.post("/send", (req, res) => {
   console.log(req.body);
   res.send("message recieved");
