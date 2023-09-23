ad4afb71b7d3c962952afc8bb1cbdad82777a415
Peter <Peterlee2094@gmail.com>
2023-09-23 15:28:17 -0500

    added styling and button to contact page render

--- client/src/components/pages/contact/renderContact.js
+++ client/src/components/pages/contact/renderContact.js
@@ -1,13 +1,21 @@
+import "../../../../public/css/contact.css";
+
 export function contactContentContainer() {
-  const contactContentContainer = document.createElement("div");
-  contactContentContainer.id = "contact-content-container";
+  const contactContainer = document.createElement("div");
+  contactContainer.id = "contact-container";
+  contactContainer.className = "container";
 
   const text = document.createElement("p");
   text.innerHTML = "contact";
+  contactContainer.appendChild(text);
 
-  contactContentContainer.appendChild(text);
+  const button = document.createElement("button");
+  button.innerHTML = "send";
+  button.id = "send-email";
+  button.class = "button";
+  contactContainer.appendChild(button);
 
-  return contactContentContainer;
+  return contactContainer;
 }
 
 export const contactRender = {
