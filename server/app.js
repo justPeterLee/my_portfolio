ed2fe9d8c4346db24046279f26de19437b2d13b4
Peter <Peterlee2094@gmail.com>
2023-09-22 20:37:15 -0500

    added node cache

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const NodeCache = require("node-cache");
 
+const cache = new NodeCache();
 const app = express();
+
 const PORT = process.env.PORT || 5001;
 
 app.use("/api/v1/", express.static("./public"));
@@ -10,6 +13,19 @@ app.use("/api/v1/", express.static("./public"));
 app.get("/api/v1/", (req, res) => {
   res.send("hello");
 });
+
+// app.get("/api/v1/project", (req, res) => {
+//   const dataKey = "prjectKey";
+//   const cacheData = cache.get(dataKey);
+
+//   if(cacheData){
+//     console.log('data served from cache');
+//     res.json(cacheData)
+//   }else{
+//     const dataFilePath = path.join(__dirname, "public", "")
+//   }
+//   res.send("hello");
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
