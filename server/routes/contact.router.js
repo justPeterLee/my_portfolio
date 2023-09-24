9cba463120366a379023b061d43c76e876f9de98
Peter <Peterlee2094@gmail.com>
2023-09-23 20:14:19 -0500

    set up nodemailer transporter

--- server/routes/contact.router.js
+++ server/routes/contact.router.js
@@ -1,18 +1,33 @@
 const express = require("express");
 const router = express.Router();
 
+require("dotenv").config();
 const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.transporter({
+const transporter = nodemailer.createTransport({
   host: "smtp.forwardemail.net",
   port: 587,
-  secure: true,
   auth: {
-    user: "",
-    pass: "",
+    user: process.env.mailAuth,
+    pass: process.env.mailPass,
   },
 });
+
+const test = process.env.test;
+console.log(test);
+
 router.post("/send", (req, res) => {
+  async function send() {
+    const info = await transporter.sendMail({
+      from: '"Fred Foo 👻" <foo@example.com>', // sender address
+      // to: "bar@example.com, baz@example.com", // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+    });
+  }
+
+  // send().catch(console.error);i
   console.log(req.body);
   res.send("message recieved");
 });
