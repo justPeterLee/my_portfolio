06814c55e434c8ccdab59b3e66991ed1f3c6d346
Peter <Peterlee2094@gmail.com>
2023-09-24 18:02:07 -0500

    change sender form

--- server/routes/contact.router.js
+++ server/routes/contact.router.js
@@ -7,27 +7,25 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp.forwardemail.net",
   port: 587,
+  secure: false,
   auth: {
     user: process.env.mailAuth,
     pass: process.env.mailPass,
   },
 });
 
-const test = process.env.test;
-console.log(test);
-
 router.post("/send", (req, res) => {
   async function send() {
     const info = await transporter.sendMail({
-      from: '"Fred Foo 👻" <foo@example.com>', // sender address
-      // to: "bar@example.com, baz@example.com", // list of receivers
+      from: `${process.env.mailAuth}`, // sender address
+      to: `${process.env.mailAuth}, ${process.env.gmailTest}`, // list of receivers
       subject: "Hello ✔", // Subject line
       text: "Hello world?", // plain text body
       html: "<b>Hello world?</b>", // html body
     });
   }
 
-  // send().catch(console.error);i
+  send().catch(console.error);
   console.log(req.body);
   res.send("message recieved");
 });
