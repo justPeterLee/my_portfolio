6554a70ef92f08e06d4c29763a1616a136910139
Peter <Peterlee2094@gmail.com>
2023-09-25 20:31:32 -0500

    fixed naming bug

--- client/src/components/pages/contact/renderContact.js
+++ client/src/components/pages/contact/renderContact.js
@@ -5,20 +5,20 @@ export function contactContentContainer() {
   contactContainer.id = "contact-container";
   contactContainer.className = "container";
 
-  const text = document.createElement("p");
-  text.innerHTML = "contact";
-  contactContainer.appendChild(text);
+  // const text = document.createElement("p");
+  // text.innerHTML = "contact";
+  // contactContainer.appendChild(text);
 
-  const button = document.createElement("button");
-  button.innerHTML = "send";
-  button.id = "send-email";
-  button.class = "button";
-  contactContainer.appendChild(button);
+  // const button = document.createElement("button");
+  // button.innerHTML = "send";
+  // button.id = "send-email";
+  // button.class = "button";
+  // contactContainer.appendChild(button);
 
-  button.addEventListener("click", () => {
-    console.log("clicked");
-    test();
-  });
+  // button.addEventListener("click", () => {
+  //   console.log("clicked");
+  //   test();
+  // });
 
   letterContainer(contactContainer);
 
@@ -30,7 +30,6 @@ function letterContainer(parent) {
     "Howdy Peter,\n I think your website is super duper cool.\n I would love to get in contact with you.\n Please reply to the email at you earliest convince.\n thank you kindly, ";
 
   let letterArray = letter.split("\n");
-  console.log(letterArray);
 
   const textContainer = document.createElement("h3");
   textContainer.id = "letter-text-container";
@@ -38,7 +37,7 @@ function letterContainer(parent) {
 
   letterArray.forEach((string, index) => {
     const hiddenContainer = document.createElement("span");
-    hiddenContainer.className = "hidden-text";
+    hiddenContainer.className = "letter-hidden-text";
     textContainer.appendChild(hiddenContainer);
 
     const stringContainer = document.createElement("span");
