835f4b78d8dad362522b5ac6b9b3043ba04a6ade
Peter <Peterlee2094@gmail.com>
2023-09-25 19:51:00 -0500

    create letter generator

--- client/src/components/pages/contact/renderContact.js
+++ client/src/components/pages/contact/renderContact.js
@@ -19,7 +19,30 @@ export function contactContentContainer() {
     console.log("clicked");
     test();
   });
+
+  letterContainer(contactContainer);
+
   return contactContainer;
+}
+
+function letterContainer(parent) {
+  const letter =
+    "Howdy Peter,\n I think your website is super duper cool.\n I would love to get in contact with you.\n Please reply to the email at you earliest convince.\n thank you kindly, ";
+
+  let letterArray = letter.split("\n");
+  console.log(letterArray);
+
+  const textContainer = document.createElement("h3");
+  textContainer.id = "letter-text-container";
+  parent.appendChild(textContainer);
+
+  letterArray.forEach((string, index) => {
+    const stringContainer = document.createElement("span");
+    stringContainer.id = `letter-words-${index}`;
+    stringContainer.className = "letter-words";
+    stringContainer.innerHTML = string;
+    textContainer.appendChild(stringContainer);
+  });
 }
 
 async function test() {
